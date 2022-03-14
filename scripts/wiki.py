b72039130e465d8fe7532b6877290cafb634d05b
Tan Çetiner <cetinertan13@gmail.com>
2022-03-14 16:38:45 +0200

    modified wiki.py
    wiki.py now can get the text data from wiki page and write it in a txt file

--- scripts/wiki.py
+++ scripts/wiki.py
@@ -1,19 +1,30 @@
 import wikipediaapi
+import os
+import re
 
 print('imported')
-
 
 wiki_wiki = wikipediaapi.Wikipedia(
         language='tr',
         extract_format=wikipediaapi.ExtractFormat.WIKI
 )
 
+topic_list = ['asal sayılar' , 'doğal sayılar', 'karmaşık sayılar', 'toplama', 'çarpma', 'kümeler', 'modüler aritmetik']
 
-page_py = wiki_wiki.page('asal sayılar')
-if page_py.exists():
-    print(page_py.summary)
-else:
-    print('Page does not exist')
+file_path = os.path.dirname(__file__)
+
+for topic in topic_list:
+    page = wiki_wiki.page(topic)
+    print(topic, ": " , page.exists())
+
+    full_path = os.path.join(file_path, '../files/input_wiki_txt/{}.txt'.format(topic))
+    if page.exists():
+        with open(full_path, 'w', encoding='utf-8') as f:
+            # f.write(''.join(page.text.split()))
+            string = " ".join(re.split("\s+", page.text, flags=re.UNICODE))
+            string = re.sub(r'(\\+)[a-z]*', '', string)
+            f.write(string)
+
 
 
 
