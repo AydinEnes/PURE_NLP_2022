e2f6158f3c7c0b9c5c65fe3450dbdaaa65989dc4
akbulutdora <akbulutdora@gmail.com>
2022-03-15 20:34:52 +0300

    train fasttext model with first group ofdata

--- scripts/fasttext_model.py
+++ scripts/fasttext_model.py
@@ -10,6 +10,7 @@ Output:
 Output format:
 
 """
+import os
 from os import path
 from base_path import PROJECT_PATH
 from spacy.lang.tr.stop_words import STOP_WORDS
@@ -17,6 +18,11 @@ from spacy.lang.tr.stop_words import STOP_WORDS
 def tokenize(sentence):
     return [token for token in sentence.split() if token not in STOP_WORDS]
 
+# return each file as a list of strings
+def get_sentences(file):
+    return tokenize(file.read())
+
+# read every file in the folder input_text_wiki and create a list for each file
 def get_sentences(filename):
     sentence_list = []
     with open(filename, encoding='utf-8') as fi:
@@ -25,8 +31,55 @@ def get_sentences(filename):
         sentence_list.append(row)
     
     return sentence_list
- 
-input_file_name = "asal sayılar.txt"
-filename= path.join(PROJECT_PATH, "files", "input_wiki_txt" , input_file_name)
-sentences=get_sentences(filename)
-print(len(sentences))
+      
+filename="wiki_texts.txt"
+filepath = path.join(PROJECT_PATH, "files", "input_wiki_txt", filename)
+sentences=get_sentences(filepath)
+# print(sentences[0])
+
+from gensim.models.phrases import Phrases, Phraser
+def build_phrases(sentences):
+    phrases = Phrases(sentences,
+                      min_count=5,
+                      threshold=1,
+                      progress_per=1000)
+    return Phraser(phrases)
+
+biagrams=build_phrases(sentences)
+corpus_with_biagrams=biagrams[sentences]
+
+
+def build_phrases2(sentences):
+    phrases = Phrases(biagrams[sentences],
+                      min_count=5,
+                      threshold=1,
+                      progress_per=1000)
+    return Phraser(phrases)
+
+trigrams=build_phrases2(sentences)
+corpus_with_trigrams=trigrams[corpus_with_biagrams]
+# print((corpus_with_trigrams[0]))
+
+# biagrams.save('biagram_model_ds.txt')
+# trigrams.save('triagram_model_ds.txt')
+#phrases_model= Phraser.load('phrases_model.txt')
+
+def create_new_corpus(filename):
+    new_corpus= []
+    with open(filename, encoding='utf-8') as fi:
+      for line in fi:
+        sent=tokenize(line)
+        recieved=(biagrams[sent])
+        recieved2=trigrams[recieved]
+        new_corpus.append(recieved2)
+    
+    return new_corpus
+    
+corpus=create_new_corpus(filepath)
+# print(corpus[1])
+
+from gensim.models import FastText
+# model_ted = FastText(corpus, size=100, window=15, min_count=5,iter=10, workers=10,sg=1)
+model_ted = FastText(corpus,vector_size =100, window=15, min_count=5,epochs=10, workers=10,sg=1)
+
+print(model_ted.wv.most_similar("asal_sayılar",topn=50))
--- scripts/wiki.py
+++ scripts/wiki.py
@@ -1,3 +1,4 @@
+from base_path import PROJECT_PATH
 import wikipediaapi
 import os
 import re
@@ -17,14 +18,16 @@ for topic in topic_list:
     page = wiki_wiki.page(topic)
     print(topic, ": " , page.exists())
 
-    full_path = os.path.join(file_path, '../files/input_wiki_txt/{}.txt'.format(topic.replace(' ', '_')))
+    # full_path = os.path.join(file_path, '../files/input_wiki_txt/{}.txt'.format(topic.replace(' ', '_')))
+    full_path = os.path.join(PROJECT_PATH, "files", "input_wiki_txt", "wiki_texts.txt")
     if page.exists():
-        with open(full_path, 'w', encoding='utf-8') as f:
+        with open(full_path, 'a', encoding='utf-8') as f:
             string = " ".join(re.split("\s+", page.text, flags=re.UNICODE))
             string = string.lower()
             string = re.sub(r'(\\+)[a-z]*', '', string)
+            string = string.translate(str.maketrans('', '', "()\{\}[]=+-.,?!"))
             string = string.split('ayrıca bakınız')[0].split('kaynakça')[0]
-            f.write(string)
+            f.write(string + "\n")
 
 
 
