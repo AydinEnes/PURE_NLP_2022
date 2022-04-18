1fb4bd45c679594a4b187700609ec1096bc9a9cf
Tan Çetiner <cetinertan13@gmail.com>
2022-04-18 17:57:42 +0300

    maths and physics texts from eba are combined in a file

--- scripts/preprocessing/bio_combine_txt.py
+++ scripts/preprocessing/bio_combine_txt.py
@@ -1,8 +1,18 @@
 from numpy import unicode_
-from base_path import PROJECT_PATH
+
+# from base_path import PROJECT_PATH
 import os
 import re
 
+import sys
+
+sys.path.insert(0, "/Users/tancetiner/Documents/GitHub/PURE_NLP_2022/scripts")
+
+from base_path import base_path
+
+
+import os
+from os import path
 
 # charlist2 = [
 #     "(",
@@ -111,18 +121,24 @@ charlist = [
 inputPaths = ["input_eba_ogm_bio_txt", "input_eba_ogm_chem_txt"]
 outputPaths = ["eba_bio_texts.txt", "eba_chem_texts.txt"]
 
+inputPaths2 = ["input_eba_ogm_physics_txt", "input_eba_ogm_maths_txt"]
+outputPaths2 = [
+    os.path.join("physics_texts", "eba_physics_texts.txt"),
+    os.path.join("maths_texts", "eba_maths_texts.txt"),
+]
+
 for i in range(2):
     with open(
         os.path.join(
-            PROJECT_PATH,
-            "files/model_input_texts/output_eba_ogm_txt",
-            outputPaths[i],
+            base_path.PROJECT_PATH,
+            "files/model_input_texts",
+            outputPaths2[i],
         ),
         "w",
         encoding="utf-8",
     ) as w:
         for subdir, dirs, files in os.walk(
-            os.path.join(PROJECT_PATH, "files", inputPaths[i])
+            os.path.join(base_path.PROJECT_PATH, "files", inputPaths2[i])
         ):
             for file in files:
 
