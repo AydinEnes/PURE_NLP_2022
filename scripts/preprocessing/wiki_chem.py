5894e6660928f2425667185d465c082160823adb
Aydın Enes <aydinenes4221@gmail.com>
2022-04-19 14:47:52 +0300

    scrape wiki chem

--- scripts/preprocessing/wiki_chem.py
+++ scripts/preprocessing/wiki_chem.py
@@ -1,11 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 import sys
-sys.path.insert(0, 'D:\Github_Repos\morpa_nlp\PURE_NLP_2022\scripts')
+
+sys.path.insert(0, '/Users/aydo/Desktop/pure/PURE_NLP_2022/scripts')
 import os
 from os import path
+
 from numpy import full
 from base_path import base_path
 import wikipediaapi
 import re
+
+
 import time
 start_time = time.time()
 
@@ -16,8 +23,8 @@ wiki_wiki = wikipediaapi.Wikipedia(
 )
 
 file_path = os.path.dirname(__file__)
-full_path = os.path.join(base_path.PROJECT_PATH, "files", "input_wiki_txt", "wiki_bio_texts2.txt")
-full_path_titles = os.path.join(base_path.PROJECT_PATH, "files", "input_wiki_txt", "wiki_bio_texts2_titles.txt")
+full_path = os.path.join(base_path.PROJECT_PATH, "files", "model_input_texts", "chem_texts", "wiki_chem_texts.txt")
+full_path_titles = os.path.join(base_path.PROJECT_PATH, "files", "model_input_texts", "chem_texts", "wiki_chem_titles.txt")
 
 with open(full_path, "w", encoding="utf-8") as f:
     print("file created")
@@ -50,7 +57,7 @@ def findSubCats(categorymembers, full_path, level=0, max_level=2):
                 name_dict[c.title] = 1
                 writeToText(c.title, full_path)
 
-cats = "Biyoloji Terminolojisi,Canlılar,Moleküler biyoloji teknikleri,Biyoloji tarihi,Bilimsel sınıflandırma,Biyoloji sistemleri,Biyoloji kavramları,Biyolojik etkileşimler,Biyolojinin genel alanları"
+cats = "Kimya Taslakları,Kimyagerler,Atmosfer kimyası,Kimyasal bağlar,Kimyanın dalları,Kimya endüstrisi,Kimyasal formüller,Kimya güvenliği,Kimya yasaları,Kimya teorileri,Metalurji,Kimya mühendisliği,Kimyasal özellikler,Kimyasal savaş,Kimya tarihi,Kimyasal maddeler"
 cats_list = cats.split(",")
 for category in cats_list:
     cat = wiki_wiki.page("Category:{}".format(category))
